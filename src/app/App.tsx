087ae0e304e85523a3d66c986a6edcaa6dbe0ff0
Dmitry Slugin <ethernalfoxd@gmail.com>
2025-01-08 22:22:54 +0300

    add scene

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,24 @@
+import type { Stage as StageType } from 'konva/lib/Stage';
+import { useEffect, useRef, useState } from 'react';
+import { Layer,  Stage } from 'react-konva';
+import Sidebar from '../features/Sidebar/Sidebar';
+import { useShapeStore } from '../entities/shapes/shapesStore';
+
 function App() {
-  return <></>;
+  const layerRef = useRef(null);
+
+  const { shapes } = useShapeStore();
+
+  console.log(shapes)
+
+  return (
+    <>
+      <Stage width={window.innerWidth} height={window.innerHeight - 100}>
+        <Layer ref={layerRef}>{...shapes}</Layer>
+      </Stage>
+      <Sidebar />
+    </>
+  );
 }
 
 export default App;
